ec1a418a2a994ba1d3678fff665fa48c9c7988b2
Abishazaigo <you@example.com>
2021-02-16 18:24:44 +0530

    Header part and the form is completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './Styles/Sb.css';
+
+import Header from'./Header';
+import Form from './Form';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Form />    
+    </>         
+  );
+}
+
+export default App;
+
